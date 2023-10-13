e69d7dd57abc15ca2216f52bdaeddf77322f0687
Parantapa Bhattacharya <pb@parantapa.net>
2023-10-13 16:14:49 -0400

    Update python module to include tree-sitter parser

--- src/sqlpygen/tree_sitter_bindings.py
+++ src/sqlpygen/tree_sitter_bindings.py
@@ -1,34 +1,22 @@
 """Python bindings for the tree-sitter-parser."""
 
-import os
 from pathlib import Path
 from tree_sitter import Language, Parser
 
 from platformdirs import user_cache_dir
 
-
-def get_tree_stter_dir() -> Path:
-    try:
-        tree_sitter_dir = os.environ["SQLPYGEN_TREE_SITTER_DIR"]
-    except Exception as e:
-        raise RuntimeError(
-            "Environment variable SQLPYGEN_TREE_SITTER_DIR not set."
-        ) from e
-
-    tree_sitter_dir = Path(tree_sitter_dir)
-    return tree_sitter_dir
-
+LANGUAGE = "sqlpygen"
+TREE_SITTER_DIR = Path(__file__).parent / f"tree-sitter-{LANGUAGE}"
 
 def get_parser() -> Parser:
-    state_dir = user_cache_dir(appname="sqlpygen")
+    state_dir = user_cache_dir(appname=LANGUAGE)
     state_dir = Path(state_dir)
     state_dir.mkdir(parents=True, exist_ok=True)
 
-    tree_sitter_dir = get_tree_stter_dir()
     library_file = state_dir / "languages.so"
 
-    Language.build_library(str(library_file), [str(tree_sitter_dir)])
-    language = Language(str(library_file), "sqlpygen")
+    Language.build_library(str(library_file), [str(TREE_SITTER_DIR)])
+    language = Language(str(library_file), LANGUAGE)
 
     parser = Parser()
     parser.set_language(language)
